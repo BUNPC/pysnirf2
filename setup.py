25eff4b3623e6fb3f9fbee797e37c6da885658a5
stephen scott tucker <sstucker@bu.edu>
2021-11-19 18:02:35 -0500

    Docs (#3)
    * Gen init commit
    
    * Update README.md
    
    * Init 2. Pretty printing, dynamic loading
    
    * Base init 2
    
    * Robust typing on read and write
    
    * Snirf-level save
    
    * Save broken but indexed groups working
    
    * Fixed save; requirements
    
    * Setup

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup
-import os
-import sys
 
 setup(
     name='pysnirf2',
     # version='0.0.1',
-    description='Compute vibrational levels, wavefunctions, and expectation values using the Numerov-Cooley algorithm.',
-    long_description=long_description,
-    author='Radovan Bast',
-    author_email='radovan.bast@uit.no',
-    url='https://github.com/bast/numerov',
-    license='MPL-2.0',
-    packages=['numerov'],
+    description='Interface and validator for the SNIRF file format dynamically generated from the specification document.',
+    author_email='sstucker@bu.edu',
+    url='https://github.com/BUNPC/pysnirf2',
+    packages=['src'],
     install_requires=[
         'cached-property==1.5.2',
         'h5py==3.6.0',
@@ -19,12 +14,8 @@ setup(
         'pip==21.3.1',
         'setuptools==40.8.0'
     ],
-    scripts=['bin/cooley'],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
     ],
 )
