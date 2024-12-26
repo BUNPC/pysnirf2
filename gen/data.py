6e40d60a671526e90ca4a5cff073403e6d1861c3
sstucker <sstucker@nyu.edu>
2024-12-26 13:38:49 -0500

    1.2-draft gen

--- gen/data.py
+++ gen/data.py
@@ -1,5 +1,5 @@
-SPEC_SRC = 'https://raw.githubusercontent.com/fNIRS/snirf/v1.1/snirf_specification.md'
-SPEC_VERSION = 'v1.1'  # Version of the spec linked above
+SPEC_SRC = 'https://raw.githubusercontent.com/fNIRS/snirf/refs/heads/master/snirf_specification.md'
+SPEC_VERSION = 'v1.2-draft'  # Version of the spec linked above
 
 """
 These types are fragments of the string codes used to describe the types of
